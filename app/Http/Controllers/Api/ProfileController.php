8ee71aab42e88f4ff2b00cf31a883e44cdac77db
CostaX549 <costasantos896@gmail.com>
2024-05-26 19:35:03 -0300

    TikTokClone API - 26/05/2024

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use App\Http\Resources\AllPostsCollection;
+use App\Http\Resources\UserCollection;
+use App\Models\Post;
+use App\Models\User;
+
+class ProfileController extends Controller
+{
+    
+
+   
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+           $posts = Post::where("user_id", $id)->orderBy("created_at", "desc")->get();
+           $user = User::where("id", $id)->get();
+
+           return response()->json([
+            'posts' => new AllPostsCollection($posts),
+            'user' => new UserCollection($user)
+        ], 200);
+          } catch(\Exception $e) {
+             return response()->json(['error' => $e->getMessage()], 400);
+          }
+    }
+
+  
+}
